28494541cede9fd6e7f98c30f8f1666635b635b7
Natasha Verdon <natashamverdon@gmail.com>
2020-10-29 10:00:35 +0100

    dropdown

--- main.js
+++ main.js
@@ -13,6 +13,19 @@ $(document).ready(function() {
                   $('#content').html(data);
               }
           });
+  var acc = document.getElementsByClassName("accordion");
+     var i;
 
+      for (i = 0; i < acc.length; i++) {
+       acc[i].addEventListener("click", function() {
+       this.classList.toggle("activate");
+      var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+         panel.style.maxHeight = null;
+       } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+       }
+       });
+     }
 
  });
