969cada850689ab300b4fdec2c09d608905afe76
Natasha Verdon <natashamverdon@gmail.com>
2020-10-29 10:07:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
   var acc = document.getElementsByClassName("accordion");
-     var i;
+      var i;
 
       for (i = 0; i < acc.length; i++) {
-       acc[i].addEventListener("click", function() {
-       this.classList.toggle("activate");
-      var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-         panel.style.maxHeight = null;
-       } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-       }
-       });
-     }
+        acc[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var panel = this.nextElementSibling;
+          if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          }
+        });
+      }
 
   $('.hamburger__container').click(function() {
               $('.nav').toggleClass('open');
